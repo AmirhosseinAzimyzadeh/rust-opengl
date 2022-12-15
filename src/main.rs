37869c49d0cd8f359d1d7231c22044328eebbcff
Amirhossein Azimyzadeh <amirhosseinazimyzadeh2@gmail.com>
2022-12-15 18:34:29 +0330

    check external functions

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
 fn main() {
     let mut my_str = String::new();
     my_str.push_str("Hello World");
-    my_str.split_whitespace().for_each(|x| {
-        println!("{}", x);
-    });
+    my_str.split_whitespace().for_each(printer);
     println!("{}", my_str);
+}
+
+
+fn printer(x: &str) {
+    println!("your str is:{}", x);
 }
