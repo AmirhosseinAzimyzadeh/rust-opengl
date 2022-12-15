d42de526cdd22cc794f19902bc30cf08e81f7f67
Amirhossein Azimyzadeh <amirhosseinazimyzadeh2@gmail.com>
2022-12-15 14:39:50 +0330

    string tests

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
 fn main() {
-    println!("Hello, world!");
+    let mut my_str = String::new();
+    my_str.push_str("Hello World");
+    my_str.split_whitespace().for_each(|x| {
+        println!("{}", x);
+    });
+    println!("{}", my_str);
 }
