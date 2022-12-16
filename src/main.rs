18a3fca2c53efc09c1aefb038225e985b390d719
Amirhossein Azimyzadeh <amirhosseinazimyzadeh2@gmail.com>
2022-12-16 15:16:22 +0330

    use clear color

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use glium::glutin::{event::Event, event_loop::{ControlFlow, EventLoopWindowTarget}};
+use glium::{glutin::{event::Event, event_loop::{ControlFlow, EventLoopWindowTarget}}, Display};
 
 #[macro_use]
 extern crate glutin;
@@ -9,22 +9,29 @@ fn main() {
   let mut event_loop = glutin::event_loop::EventLoop::new();
   let window_builder = glutin::window::WindowBuilder::new();
   let context_builder = glutin::ContextBuilder::new();
-  let display = glium::Display::new(window_builder, context_builder, &event_loop);
+  let display = glium::Display::new(window_builder, context_builder, &event_loop).unwrap();
 
-  event_loop.run(loop_handler);
+  event_loop.run(move |e, t, cf| {
+    loop_handler(e, t, cf, &display)
+  });
 }
 
 fn loop_handler(
   event: Event<()>,
   _: &EventLoopWindowTarget<()>,
-  control_flow: &mut ControlFlow
+  control_flow: &mut ControlFlow,
+  display: &Display,
 ) {
-  use glium::glutin;
+  use glium::{glutin, Surface};
   let next_frame_time = std::time::Instant::now()
     + std::time::Duration::from_nanos(16_666_667);
 
   *control_flow = glutin::event_loop::ControlFlow::WaitUntil(next_frame_time);
 
+  let mut target = display.draw();
+  target.clear_color(0.0, 0.0, 0.0, 1.0);
+  target.finish();
+  
   match event {
     glutin::event::Event::WindowEvent { event, .. } => match event {
       glutin::event::WindowEvent::CloseRequested => {
