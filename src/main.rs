248566adff64d09746828547af1804f9c2ca548f
Amirhossein Azimyzadeh <amirhosseinazimyzadeh2@gmail.com>
2022-12-17 16:57:04 +0330

    implement triangle

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+#[macro_use]
+extern crate glium;
+
 mod vertex;
 
 use glium::{glutin::{event::Event, event_loop::{ControlFlow, EventLoopWindowTarget}}, Display};
@@ -11,8 +14,42 @@ fn main() {
   let context_builder = glutin::ContextBuilder::new();
   let display = glium::Display::new(window_builder, context_builder, &event_loop).unwrap();
 
+  let shape = vec![
+    Vertex::new([0.5, 0.5]),
+    Vertex::new([-0.5, -0.5]),
+    Vertex::new([-0.5, 0.5]),
+  ];
+
+  let vertex_buffer = glium::VertexBuffer::new(&display, &shape).unwrap();
+  let indices = glium::index::NoIndices(glium::index::PrimitiveType::TrianglesList);
+
+  let vertex_shader_src = r#"
+        #version 140
+        in vec2 position;
+        void main() {
+            gl_Position = vec4(position, 0.0, 1.0);
+        }
+    "#;
+
+  let fragment_shader_src = r#"
+        #version 140
+        out vec4 color;
+        void main() {
+            color = vec4(1.0, 0.0, 0.0, 1.0);
+        }
+    "#;
+
+  let program = glium::Program::from_source(&display, vertex_shader_src, fragment_shader_src, None).unwrap();
+
+
   event_loop.run(move |e, t, cf| {
-    loop_handler(e, t, cf, &display)
+    loop_handler(e, t, cf);
+    use glium::Surface;
+    let mut target = display.draw();
+    target.clear_color(0.0, 0.0, 1.0, 1.0);
+    target.draw(&vertex_buffer, &indices, &program, &glium::uniforms::EmptyUniforms,
+                    &Default::default()).unwrap();
+    target.finish().unwrap();
   });
 }
 
@@ -20,17 +57,12 @@ fn loop_handler(
   event: Event<()>,
   _: &EventLoopWindowTarget<()>,
   control_flow: &mut ControlFlow,
-  display: &Display,
 ) {
-  use glium::{glutin, Surface};
+  use glium::glutin;
   let next_frame_time = std::time::Instant::now()
     + std::time::Duration::from_nanos(16_666_667);
 
   *control_flow = glutin::event_loop::ControlFlow::WaitUntil(next_frame_time);
-
-  let mut target = display.draw();
-  target.clear_color(1.0, 0.0, 0.0, 1.0);
-  target.finish().unwrap();
   
   match event {
     glutin::event::Event::WindowEvent { event, .. } => match event {
