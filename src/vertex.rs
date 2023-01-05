8ac6bb4c60931bf17b7fa3769c68393e1772dc9d
Amirhossein Azimyzadeh <amirhosseinazimyzadeh2@gmail.com>
2023-01-05 17:15:22 +0330

    add texture coordinate

--- src/vertex.rs
+++ src/vertex.rs
@@ -1,12 +1,13 @@
 #[derive(Copy, Clone)]
 pub struct Vertex {
     position: [f32; 2],
+    texture_coordinate: [f32; 2],
 }
 
 impl Vertex {
-    pub fn new(position: [f32; 2]) -> Self {
-      Vertex { position }
-    }
+  pub fn new(position: [f32; 2], texture_coordinate: [f32; 2]) -> Self {
+    Vertex { position, texture_coordinate }
+  }
 }
 
 implement_vertex!(Vertex, position);
